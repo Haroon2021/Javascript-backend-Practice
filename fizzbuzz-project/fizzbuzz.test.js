bac9c751ce70dcebc2c9c705de32ce18d5f4bda9
Haroon <haroon20@hotmail.co.uk>
2022-04-26 11:48:20 +0100

    all tests passing

--- fizzbuzz-project/fizzbuzz.test.js
+++ fizzbuzz-project/fizzbuzz.test.js
@@ -7,4 +7,16 @@ describe('fizzbuzz', () => {
     it('fizzbuzz 5', () => {
         expect(fizzbuzz(5)).toBe('Buzz')
     });
+    it('fizzbuzz 8', () => {
+        expect(fizzbuzz(8)).toBe(8)
+    });
+    it('fizzbuzz 15', () => {
+        expect(fizzbuzz(15)).toBe('FizzBuzz')
+    })
+    it('fizzbuzz 18', () => {
+        expect(fizzbuzz(18)).toBe('Fizz')
+    });
+    it('fizzbuzz 20', () => {
+        expect(fizzbuzz(20)).toBe('Buzz')
+    });
 });
